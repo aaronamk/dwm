d5987484ff4fa30b78c0d4761a1a2eb66b169e21
aaronamk <amklein@email.wm.edu>
2020-08-20 23:36:24 -0400

    Applied my original patch: zoomfloating

--- dwm.c
+++ dwm.c
@@ -2451,6 +2451,11 @@ xerrorstart(Display *dpy, XErrorEvent *ee)
 void
 zoom(const Arg *arg)
 {
+	if (selmon->sel && selmon->sel->isfloating) {
+		togglefloating(NULL);
+		return;
+	}
+
 	Client *c = selmon->sel;
 
 	if (!selmon->lt[selmon->sellt]->arrange
